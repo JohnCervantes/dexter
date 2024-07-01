0fbe8511ca9187b239572ba60f9c07248ea977fe
john Cervantes <johncamilocervantes@gmail.com>
2024-07-01 16:37:20 -0500

    optimizing gallery load times

--- pages/gallery.js
+++ pages/gallery.js
@@ -155,10 +155,10 @@ export async function getServerSideProps(context) {
     }
 
     //shuffle images
-    for (let i = images.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [images[i], images[j]] = [images[j], images[i]];
-    }
+    // for (let i = images.length - 1; i > 0; i--) {
+    //   const j = Math.floor(Math.random() * (i + 1));
+    //   [images[i], images[j]] = [images[j], images[i]];
+    // }
 
     return {
       props: {
